57621b77f46c29113b17a900eff1d2f952bdbc22
Gabi <gabbiedjulabic@gmail.com>
2024-10-24 17:37:10 -0400

    Updated app.js

--- app.js
+++ app.js
@@ -1,21 +1,17 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 3000;
 
-// Serve static files (from public directory..?)
+// Basic route for testing
+app.get('/test', (req, res) => {
+  res.send('Test route working');
+});
+
+// Serve static files (index.html)
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Serve the index.html file for the root URL
-// app.get('/test', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'public', 'index.html'));
-// });
-
-app.get('/inspek-reporting-app/test', (req, res) => {
-    res.send("This is a test route");
-  });
-
-//start server
-app.listen(port, '0.0.0.0', () => {
-    console.log(`Server running at http://0.0.0.0:${port}`);
+// Start the server
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
