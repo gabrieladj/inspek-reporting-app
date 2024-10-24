eb12e14f7c9d19ca031db7a080128b89a58084ba
Gabi <gabbiedjulabic@gmail.com>
2024-10-24 11:36:32 -0400

    Added CS file

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const port = 3000; // Or any port you prefer
 
+// Serve static files (like CSS)
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
     res.send('Inspek Reporting Backend is up and running!');
 });
