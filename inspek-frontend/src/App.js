34acb95867c1a9cb0accf1788321c3d42fc4c4b3
Gabi <gabbiedjulabic@gmail.com>
2024-10-29 01:06:28 -0400

    Update CI/CD Workflow

--- inspek-frontend/src/App.js
+++ inspek-frontend/src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import './App.css';
 import Login from './components/Login.js';
-import './components/Login.css'
-import { useEffect, useState } from 'react'
+import './components/Login.css';
+import { useState } from 'react';
+//import { useEffect } from 'react'
 
 import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 
-
 function App() {
-  const [loggedIn, setLoggedIn] = useState(false)
-  const [email, setEmail] = useState('')
+  // const [loggedIn, setLoggedIn] = useState(false)
+  // const [email, setEmail] = useState('')
+
+  const [setLoggedIn] = useState(false)
+  const [setEmail] = useState('')
 
   return (
     <div className="App">
