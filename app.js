24451944da76677ae5d201fe285c52c4051f7992
Gabi <gabbiedjulabic@gmail.com>
2024-11-13 01:05:47 -0500

    CORS update

--- app.js
+++ app.js
@@ -17,17 +17,15 @@ const Report = require('./models/Report');
 const app = express();
 app.use(express.json()); 
 
-const allowedOrigins = ['http://localhost:3000', 'http://142.93.112.132'];  // Add all allowed origins
+const allowedOrigins = ['http://localhost:3000', 'http://142.93.112.132'];
 app.use(cors({
-  origin: function (origin, callback) {
-    if (allowedOrigins.includes(origin) || !origin) {
+  origin: function(origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
       callback(null, true);
     } else {
       callback(new Error('Not allowed by CORS'));
     }
-  },
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true,
+  }
 }));
 
 
